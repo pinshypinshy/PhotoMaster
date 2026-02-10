b8f287054a0d14f529e1bce768bdf41597e54df7
pinshypinshy <hrisyklit@gmail.com>
2026-02-10 15:24:28 +0900

    Initial Commit

---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by 平石悠生 on 2026/02/10.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
